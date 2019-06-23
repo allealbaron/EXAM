98b8d754b2c22fd2c541526bf38a7e9c468ddea7
allealbaron <angel.luis.leal.baron@gmail.com>
2019-06-23 17:20:45 +0200

    Added connection between rubriceditor and visor.
    Added connection between rubriceditor and visor.

--- js/rubriceditor.js
+++ js/rubriceditor.js
@@ -5,8 +5,53 @@
     $("#btnSaveRubric").click(
         function()
         {
-            localStorage.setItem("rubric", JSON.stringify(tinymce.activeEditor.getContent()));
-            alert(tinymce.activeEditor.getContent());
+          var itemsInEditor = $("<div>").append($(tinymce.activeEditor.getContent({format : 'raw'})));
+
+          $.each(itemsInEditor.find(".rubricQuestion"), 
+          function(index, element)
+          {
+            $(element).attr("id", "rubricQuestion" + index);
+          });
+
+          $.each(itemsInEditor.find(".rubricSection"), 
+          function(index, element)
+          {
+            $(element).attr("id", "rubricSection" + index);
+          });
+
+
+          $.each(itemsInEditor.find("ul"), function(index, element)
+          {
+            $(element).attr("id", "rubricUnordered" + index)
+                      .children().each(
+                        function(indexLi, elementLi)
+                        {
+                          $(elementLi)
+                          .attr("id", "rubricUnordered" + index + "rubricSection" + indexLi)
+                          .addClass("rubricUnordered rubricItem");
+                        }
+                      );
+          });
+
+          $.each(itemsInEditor.find("ol"), function(index, element)
+          {
+            $(element).attr("id", "rubricOrdered" + index)
+                      .children().each(
+                        function(indexLi, elementLi)
+                        {
+                          $(elementLi)
+                          .attr("id", "rubricordered" + index + "rubricSection" + indexLi)
+                          .addClass("rubricOrdered rubricItem");
+                        }
+                      );
+          });
+
+
+            localStorage.setItem("rubric", JSON.stringify(itemsInEditor.html()));
+
+
+
+            alert(itemsInEditor.html());
 
         }
     );
@@ -17,13 +62,13 @@
     spellchecker_language: 'es',
     selector: '#textEditor',
     toolbar: 'styleselect  | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | code removeformat ',
-    plugins: 'code print preview fullpage powerpaste searchreplace autoresize autolink directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime lists wordcount tinymcespellchecker a11ychecker imagetools textpattern help formatpainter permanentpen pageembed linkchecker',
+    plugins: 'code print preview fullpage searchreplace autoresize autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime lists wordcount imagetools textpattern help',
     style_formats: [
-      {title: 'Pregunta', block: 'h3', classes: 'rubricQuestion'},
-      {title: 'Subapartado', block: 'h4', classes: 'rubricSection'},
-      {title: 'Párrafo', block: 'p', classes: 'rubricParagraph'},
-      {title: 'Lista', block: 'span', classes: 'rubricList rubricUnordered'},
-      {title: 'Lista ordenada', block: 'span', classes: 'rubricList rubricOrdered'}
+      {title: 'Pregunta', block: 'h3', classes: 'rubricItem rubricQuestion'},
+      {title: 'Subapartado', block: 'h4', classes: 'rubricItem rubricSection'},
+      {title: 'P?rrafo', block: 'p', classes: 'rubricItem rubricParagraph'},
+      {title: 'Lista', block: 'span', classes: 'rubricItem rubricList rubricUnordered'},
+      {title: 'Lista ordenada', block: 'span', classes: 'rubricItem rubricList rubricOrdered'}
   ],
   content_css: [
       '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
--- js/visor.js
+++ js/visor.js
@@ -1,6 +1,5 @@
 var pdfContainer;
 var inputPage;
-var selectionsContainer;
 var pdfDocument;
 var filePath;
 var currentScale;
@@ -9,9 +8,11 @@ var selectionCounter;
 var selections;
 var currentColor;
 var currentOpacity;
+var selectedSelection;
+var scrollPosition;
 
 (function( $ ) {
- 
+
 	/**
 	 * @description Function to create toolbar buttons
 	 * @param labelName: Label
@@ -30,6 +31,82 @@ var currentOpacity;
 
 	};
 
+	/** 
+	* @description Create a div as a Selection
+	* @param value: JSON object with all the information required
+	*/
+
+	$.fn.drawSelection = function (value) {
+	   selectionCounter++;
+	   var newDiv = $("<div>");
+	   var initialState = (value.windowLocked=="on");
+	   newDiv
+		   .draggable({
+			   containment: pdfContainer,
+			   disabled: initialState
+		   })
+		   .resizable({
+			   containment: pdfContainer,
+			   disabled: initialState,
+			   minWidth: "350",
+			   handles: "all"
+		   })
+		   .data("checks", value.checks)
+		   .data("windowLocked", value.windowLocked)
+		   .data("qualification", value.qualification)
+		   .data("backgroundColor", value.backgroundColor)
+		   .addClass("selection")
+		   .attr("id", value.id)
+		   .css({
+			   "position": "relative",
+			   "left": (value.posIni.left) * currentScale,
+			   "top": scrollPosition + (value.posIni.top * currentScale),
+			   "width": value.width * currentScale,
+			   "height": value.height * currentScale,
+			   "opacity": value.opacity,
+			   "background-color": value.backgroundColor,
+			   "opacity": value.opacity,
+			   "z-index": 2
+		   })
+		   .click(function()
+			   {
+				   selectedSelection = $(this).attr("id");
+				   $("#slider").find("input[type=checkbox]").prop("checked",false);
+				   $.each($(this).data("checks"), function(index, value) {
+					   $("#" + value).prop("checked", true);
+				   });
+		   })
+		   .append(createButtonsBar(value))
+		   .append($("<textarea>").addClass("textAreaSelection").draggable({
+			   containment: newDiv
+		   }).val(value.comments));
+
+		   this.append(newDiv);
+
+		   newDiv.find(".spinnerQualification").spinner
+			({
+				classes: {
+				  "ui-button": "highlight"
+				},
+				label: "Nota",
+				showLabel: true,
+				min: 1,
+				max: value.qualification
+				})
+				.val(value.qualification)
+				  .on("input", function(e){
+					
+					if (!$.isNumeric($(this).val()))
+					{
+						$(this).val($(this).spinner( "option", "min" ));
+					}
+					})
+					.on("spinstop", function(e, ui){
+						if ($(this).val() != ""){
+							$("#" + value.id).data("qualification", this.value)	;
+						}})
+					.addClass("leftPos").val(value.qualification);
+   }
 }(jQuery));
 
 /**
@@ -37,6 +114,7 @@ var currentOpacity;
 */
 $(document)
 	.ready(function() {
+	
 		setUp();
 		
 		loadTask().promise.then(
@@ -46,8 +124,7 @@ $(document)
 
 		loadToolbar();
 
-		loadRightSlider();
-		
+		loadRightSlider();		
 	});
 
 /**
@@ -63,9 +140,10 @@ function setUp()
   	selectionCounter = 0;
 	selections = [];
 	var bodyStyles = window.getComputedStyle(document.body);
-	currentColor= bodyStyles.getPropertyValue("--main-bg-color");
+	currentColor= "rgb(241,249,155)";
+	scrollPosition = 0;
 	currentOpacity = bodyStyles.getPropertyValue("--main-opacity");
-
+	
 	filePath = "./examen.pdf";
   
 }
@@ -96,6 +174,9 @@ function loadFirstPage(pdfDocument_)
 
 }
 
+/*
+Shows/hides spinner (while a page is loading, is better to hide)
+*/
 function toggleSpinner(newValue)
 {
 	$(" .ui-spinner a.ui-spinner-button").css("display",newValue);
@@ -124,32 +205,29 @@ function loadPage()
 		canvas.width= viewport.width; 
 		
 		var renderContext = {
-      canvasContext: context,
-      viewport: viewport
-    };
+      		canvasContext: context,
+      		viewport: viewport
+    	};
 
 		return pdfPage.render(renderContext).promise.then(function() {
 			return pdfPage.getTextContent();
-	}).then(function(textContent) {
+		}).then(function(textContent) {
 			 
 			var pdfCanvas = $("#mycanvas"); 
 			var canvasOffset = pdfCanvas.offset();
 	
 			$("#textlayer").empty()
-										 .css({ left: canvasOffset.left + "px", 
-														top: canvasOffset.top + "px", 
-														height: pdfCanvas.get(0).height + "px", 
-														width: pdfCanvas.get(0).width + "px" });
+							.css({ left: canvasOffset.left + "px", 
+									top: canvasOffset.top + "px", 
+								 height: pdfCanvas.get(0).height + "px", 
+								  width: pdfCanvas.get(0).width + "px" });
 	
 			pdfjsLib.renderTextLayer({
 					textContent: textContent,
 					container: $("#textlayer").get(0),
 					viewport: viewport
 			});
-
-	});
-		
-
+		});
 	}).then(function(){
 			pdfContainer.css("width", pdfContainer.find(">:first-child").innerWidth());
 			if (selections!=[])
@@ -157,15 +235,14 @@ function loadPage()
 				drawSelectionsInPage();
 			}
 			toggleSpinner("block");
-		});
-	
+		});	
 }
 
+/* Searchs text in document*/
 function searchPage(doc, pageNumber, textToFind) {
   return doc.getPage(pageNumber).then(function (page) {
     return page.getTextContent();
   }).then(function (content) {
-    // Search combined text content using regular expression
     var text = content.items.map(function (i) { return i.str; }).join('');
     var re = new RegExp("(.{0,20})" + textToFind + "(.{0,20})", "gi"), m;
     var lines = [];
@@ -186,10 +263,10 @@ function drawSelectionsInPage() {
 
 	$.each(selections, function(index, value) {
 		if (value.page == inputPage.val()) {
-			pdfContainer.append(drawSelection(value));
+			pdfContainer.drawSelection(value);
 		} 
 	});
-
+	
 }
 
 /**
@@ -197,13 +274,29 @@ function drawSelectionsInPage() {
  * of selections
  */
 function saveSelectionsFromPage() {
+
+	/* Remove current selections already stored*/
+	selections = $.map(selections, function (item) 
+				{
+					if (item.page == inputPage.val()) 
+					{
+						return null;
+					}
+					else
+					{ 
+						return item;
+					}
+				});
+
 	$("#pageContainer div.selection")
 		.each(function(i) {
 			var myObject = new Object();
-			myObject.page = this.id.substring(this.id.indexOf("-") + 1);
-			myObject.posIni = $(this)
-				.position();
-			myObject.id = this.id;
+			myObject.page = inputPage.val();
+			myObject.posIni = {
+				"left": $(this).position().left / currentScale,
+				"top": $(this).position().top / currentScale
+			};
+			myObject.id = "selection" + i + "-" + inputPage.val();
 			myObject.title = $(this)
 				.find(".topBar")
 				.contents()
@@ -211,17 +304,17 @@ function saveSelectionsFromPage() {
 					return this.nodeType == Node.TEXT_NODE;
 				})
 				.text();
-			myObject.posFin = {
-				"left": myObject.posIni.left + $(this)
-					.width(),
-				"top": myObject.posIni.top + $(this)
-					.height()
-			};
-			myObject.backgroundColor = $(this).css("background-color");
+			myObject.width = $(this).width() / currentScale;
+			myObject.height = $(this).height()/ currentScale;
+			myObject.backgroundColor = $(this).data("backgroundColor");
 			myObject.opacity = $(this).css("opacity");
-			myObject.comments = $(this).find("textArea:first").val(); 
+			myObject.comments = $(this).find("textArea:first").val();
+			myObject.checks = $(this).data("checks");
+			myObject.windowLocked = $(this).data("windowLocked");
+			myObject.qualification = $(this).data("qualification");
 			selections.push(myObject);
 		});
+		alert("Items saved");
 }
 
 /**
@@ -232,33 +325,30 @@ function loadToolbar()
 
 	$("#toolbar").draggable();
 
+	createSpinnerPageNumber();
+
 	$("#addIcon")
 		.createToolBarButton("New Selection", "ui-icon-plus")
 		.click(function() {
 			addSelection();
 	});
 	
-	createSpinnerPageNumber();
-	
 	$("#loadIcon")
 	.createToolBarButton("Load selections", "ui-icon-script")
 	.click(function() {
-		selections = JSON.parse(localStorage.getItem("selections"));
-		drawSelectionsInPage();
+		loadSelections();
 	});
 
 	$("#saveIcon")
 	.createToolBarButton("Save selections", "ui-icon-disk")
 	.click(function() {
-		saveSelectionsFromPage();
-		localStorage.setItem("selections", JSON.stringify(selections));
+		saveSelections();
 	});
 
 	$("#zoomInIcon")
 	.createToolBarButton("Zoom in", "ui-icon-zoomin")
 	.click(function() {
 		zoomIn();
-		console.log(searchPage(pdfDocument, 1, "Trace"));
 	});
 	
 	$("#zoomOutIcon")
@@ -273,6 +363,20 @@ function loadToolbar()
 		$("#slider").slideReveal("toggle");
 	});
 		
+}
+
+/* Loads stored selections */
+function loadSelections()
+{
+	selections = JSON.parse(localStorage.getItem("selections"));
+	drawSelectionsInPage();
+}
+
+/* Saves selections */
+function saveSelections()
+{
+	saveSelectionsFromPage();
+	localStorage.setItem("selections", JSON.stringify(selections));
 }
 
 /* Loads the right slider panel */
@@ -293,9 +397,63 @@ function loadRightSlider()
 					},
 		push: false
 	}).append(
-		JSON.parse(localStorage.getItem("rubric")));
-
+		JSON.parse(localStorage.getItem("rubric")
+		));
+		$(".rubricItem.rubricQuestion, .rubricItem.rubricSection").prepend(
+			function()
+			{
+				return createCheckBox(this);
+			}
+		);
+		$(".rubricItem.rubricUnordered, .rubricItem.rubricOrdered").prepend(
+			function()
+			{
+				return createCheckBox(this);
+			}
+		);
 }
+
+/* Appends a check box to every item in the rubric */
+function createCheckBox(item)
+{
+	return $("<input/>")
+		.attr(
+			{
+					type:"checkbox",
+					id: "chk" + $(item).attr("id"),
+			})
+		.change(function()
+		{
+			if (selectedSelection != null)
+			{
+				var selection = $("#" + selectedSelection);
+				var checkedItems = [];
+
+				if (selection.data("checks"))
+				{
+					checkedItems = selection.data("checks");
+				}
+				if (this.checked)
+				{
+					checkedItems.push($(this).attr("id"));
+				}
+				else
+				{
+					var valueToFind = $(this).attr("id");
+					var itemToRemove = 0;
+					$.each(checkedItems, function(i, val)
+					{
+						if (val == valueToFind)
+						{
+							itemToRemove = i;
+						}
+					});
+					checkedItems.splice(itemToRemove,1);
+				}
+				selection.data("checks",  checkedItems);
+			}
+		});
+	}
 
 /**
  * @description Creates the spinner control
@@ -313,10 +471,8 @@ function createSpinnerPageNumber()
     min: 1
 	})
 	.val(1)
-  .on("input", function(e){
+  	.on("input", function(e){
 		
-		//var v = parseInt($(this).val());
-
 		if (!$.isNumeric($(this).val()))
 		{
 			$(this).val($(this).spinner( "option", "min" ));
@@ -359,111 +515,149 @@ loadPage();
  */
 function addSelection() 
 {
+  pdfContainer.drawSelection({
+		"id": "selection" + selectionCounter + "-" + inputPage.val(),
+		"title": "Selection " + inputPage.val() + " - " + selectionCounter,
+		"backgroundColor": currentColor,
+		"opacity": currentOpacity,
+		"page": inputPage.val(),
+		"checks": [],
+		"posIni": {
+			"left": pdfContainer.position().left,
+			"top": pdfContainer.position().top
+		},
+		height: 400,
+		width: 300,
+		"comments":"",
+		"windowLocked":"off",
+		"selectionCounter":selectionCounter,
+		"qualification": 100
+		});
 
-  var newId = "selection" + selectionCounter + "-" + inputPage.val();
+	}
 
-  selectionCounter++;
-	
-	pdfContainer
-		.append(drawSelection({
-			"id": newId,
-			"title": "Selection " + inputPage.val() + " - " + selectionCounter,
-			"backgroundColor": currentColor,
-			"opacity": currentOpacity,
-			"page": inputPage.val(),
-			"posIni": {
-				"left": pdfContainer.position().left,
-				"top": pdfContainer.position().top
-			},
-			"posFin": {
-				"left": pdfContainer.position().left + 200,
-				"top":  pdfContainer.position().top + 100
-			},
-			"comments":""
-		}));
-
-}
-
-/** 
- * @description Create a div as a Selection
- * @param value: JSON object with all the information required
- */
-function drawSelection(value) {
-	var newDiv = $("<div>");
-	return newDiv
-		.draggable({
-			containment: pdfContainer
-		})
-		.resizable({
-			containment: pdfContainer,
-			handles: "all"
-		})
-		.addClass("selection")
-		.attr("id", value.id)
-		.css({
-			"position": "absolute",
-			"left": (value.posIni.left) * currentScale,
-			"top": (value.posIni.top) * currentScale,
-			"width": (value.posFin.left - value.posIni.left) * currentScale,
-			"height": (value.posFin.top - value.posIni.top) * currentScale,
-			"opacity": value.opacity,
-			"background-color": value.backgroundColor,
-			"opacity": value.opacity,
-			"z-index": 2
-		})
-		.append(createButtonsBar())
-		.append($("<textarea>").addClass("textAreaSelection").draggable({
-			containment: newDiv
-		}).val(value.comments));
-}
-
-function createButtonsBar()
+/* Creates buttons bar for a selection */
+function createButtonsBar(value)
 {
-	var newDiv = $("<div>");
-	return newDiv
-		.append($("<button>")
-			.createToolBarButton("Select Color", "ui-icon-transfer-e-w")
-			.click(
-				function()
+	var iconSelectedInitial;
+	iconSelectedInitial = "ui-icon-unlocked";
+	if (value.windowLocked== "on")
+	{
+		iconSelectedInitial = "ui-icon-unlocked";
+	}
+	return $("<div>").append($("<input>")
+					.attr("id", value.id + "colorPicker")
+					.attr("type", "color")
+					.addClass("leftPos")
+					.css(
+						{    
+							"padding":"0.1em",
+							"width":"2em",
+							"height":"2em"}
+					)
+					.val(rgb2hex(value.backgroundColor))
+					.button()
+						.on("input", function(e){
+							$("#" + value.id).css("background-color", $(this).val());
+							currentColor = $(this).val();
+							$("#" + value.id).data("backgroundColor",$("#" + value.id).css("background-color")	);
+						}))
+				.append($("<button>")
+					.createToolBarButton("Connect", "ui-icon-tag")
+					.addClass("leftPos")
+					.click(
+						function()
+						{
+							$("#slider").slideReveal("show");
+						}))
+				.append($("<button>")
+					.createToolBarButton("Block/Unblock", iconSelectedInitial)
+					.addClass("leftPos")
+					.click(
+						function()
+						{
+							var toggleVar, iconSelected;
+							toggleVar = "on";
+							iconSelected = "ui-icon-locked";
+							if ($("#" + value.id).data("windowLocked")== "on")
+							{
+								toggleVar = "off";
+								iconSelected = "ui-icon-unlocked";
+							}
+							$(this).button({
+								icon: iconSelected
+							});
+							var box = $("#" + value.id);
+							var newStatus = !(box.draggable("option", "disabled"));
+							box.data("windowLocked", toggleVar)
+								 .draggable({disabled:newStatus})
+								 .resizable({disabled:newStatus});
+						}))
+			.append(addQualification(value).addClass("leftPos"))
+			.append($("<button>")
+				.createToolBarButton("Close", "ui-icon-close")
+				.addClass("rightPos")
+				.click(
+				function(e)
 				{
-					this.append($("<div>")
-							.append($("<label>").attr("for", "colorPicker").text("Select a color"))
-							.append($("<input>")
-								.attr("id", "colorPicker")
-								.attr("type", "color")
-								.val(currentColor)
-								.on("input", function(e){
-									newDiv.parent().css("background-color", $(this).val());
-									currentColor = $(this).val();
-								}))
-							.append(createSlider(newDiv.parent()))
-					.dialog({
-						closeOnEscape: true,
-						modal:true
-					}));
-				}
-			)
-		);
+					e.preventDefault();
+					$("#dialog").dialog({
+						buttons : {
+						  "Confirmar" : function() {
+							$("#"+value.id).remove();
+							$(this).dialog("close");  
+						  },
+						  "Cancelar" : function() {
+							$(this).dialog("close");
+						  }
+						}
+					  });
+				}))
+				.append(createSlider(value).addClass("rightPos"));
 }
-
 
 /**
  * @description Creates a slider
  */
 function createSlider(value){
 
-	return $("<div>")
+	return ($("<div>").css({width: "5em","marginRight":"1.2em", "marginTop":"0.3em"})
 	.slider({
 		max: 1,
 		min: 0.30,
 		step: 0.01,
-		value: currentOpacity,
+		value: value.opacity,
 		slide: function(event, ui) {
 			currentOpacity = ui.value;
-			value
+			$("#"+value.id)
 				.css({
 				opacity: ui.value
 			})
 		}
-});
+	})
+	);
+
 }
+
+/**
+ * @description Creates a slider
+ */
+function addQualification(item){
+
+	return $("<input>").addClass("spinnerQualification").attr("type","text");
+	
+}
+
+/* Aux Function to convert rgb color to hex format */
+function ToHex(x) {
+	var hexDigits =["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]; 
+	  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+}
+
+/* Function to convert rgb color to hex format */
+function rgb2hex(rgb) {
+	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+	return "#" + ToHex(rgb[1]) + ToHex(rgb[2]) + ToHex(rgb[3]);
+}
+	
+
